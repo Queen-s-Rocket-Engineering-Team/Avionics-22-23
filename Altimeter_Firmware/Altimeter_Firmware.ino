e29a06111af0e72c470971104e08d34124cfdd5f
Kenneract <37283040+Kenneract@users.noreply.github.com>
2023-05-23 16:59:55 -0400

    Basic pressure and temperature sensor interfaces
    Still needs some work. May switch to other MS5x library to use built-in oversampling & altitude functions (if can get it working reliably)

--- Altimeter_Firmware/Altimeter_Firmware.ino
+++ Altimeter_Firmware/Altimeter_Firmware.ino
@@ -1,10 +1,17 @@
+#include <Wire.h>
+//https://reference.arduino.cc/reference/en/libraries/ms5xxx/
+#include <MS5xxx.h>
+
 #define BATT_SENSE_PIN PA2
 #define STATUS_LED_PIN PA0
 #define FIRE_1_PIN PB7 //name based on SILKSCREEN
 #define FIRE_2_PIN PB6 //name based on SILKSCREEN
 #define CONTINUITY_1_PIN PB9 //name based on SILKSCREEN
 #define CONTINUITY_2_PIN PB8 //name based on SILKSCREEN
+#define SCL_PIN PB10
+#define SDA_PIN PB11
 
+//TODO: Add internal filtering (median?) to getTemp and getPressure functions.
 
 // Battery sense voltage divider ratio (default 3.00x)
 const float BATT_SENSE_MULTIPLIER = 3.012; //199.4k & 99.1k
@@ -18,6 +25,15 @@ unsigned int flashTime = 190;                     // Time (ms) to hold flashes
 unsigned long lastFlashChange = 0;                // Last millis() timestamp that a flash change occured
 byte statusFlashRemain = 0;                       // Number of status flashes remaining in current cycle
 byte statusCode = 0;                              // Status LED will flash this many times
+
+// Variables; pressure sensor
+double cachedTemp = 0; // Last-read temperature (0.01C)
+double cachedPres = 0; // Last-read pressure (Pa)
+const int TEMP_OFFSET = 0; // Offset for temperature (1 = 0.01C)
+const int PRES_OFFSET = 0; // Offset for pressure (1 = 1Pa)
+
+// Global objects
+MS5xxx ms5607Sensor(&Wire);
 
 
 
@@ -95,7 +111,38 @@ void handleStatusFlash() {
 }//handleStatusFlash()
 
 
+/*
+ * Polls new data from the pressure sensor.
+ * Caches values for later use.
+ * 
+ * Uses moderate resources (~30ms & 48MHz).
+ */
+void pollPressureSensor() {
+  ms5607Sensor.ReadProm();
+  ms5607Sensor.Readout();
+  cachedTemp = ms5607Sensor.GetTemp();
+  cachedPres = ms5607Sensor.GetPres();
+}//pollPressureSensor
 
+/*
+ * Returns current temperature in
+ * celcius.
+ * 
+*  No filtering is applied (yet)
+ */
+double getTemp() {
+  return (cachedTemp+TEMP_OFFSET)/100.0d;
+}//getTemp()
+
+/* 
+ * Returns current pressure in
+ * pascals.
+ *  
+ * No filtering is applied (yet)
+ */
+double getPressure() {
+  return cachedPres+PRES_OFFSET;
+}//getPressure()
 
 
 
@@ -117,23 +164,35 @@ void handleStatusFlash() {
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // Start serial (Baud doesn't matter for STM32 USB)
+  Serial.begin(9600);
+  // Configure pinmodes
   pinMode(BATT_SENSE_PIN, INPUT_ANALOG);
   pinMode(STATUS_LED_PIN, OUTPUT);
   pinMode(FIRE_1_PIN, OUTPUT);
   pinMode(FIRE_2_PIN, OUTPUT);
   pinMode(CONTINUITY_1_PIN, INPUT);
   pinMode(CONTINUITY_2_PIN, INPUT);
-   
+  // Force ADC to max resolution
   analogReadResolution(12);
-
-  Serial.begin(9600); //Baud doesn't matter for STM32 USB
+  // Configure I2C
+  Wire.setSCL(SCL_PIN);
+  Wire.setSDA(SDA_PIN);
+  // Connect to MS5607 Pressure Sensor
+  while (ms5607Sensor.connect() > 0) {
+    Serial.println(F("Connecting to MS5607..."));
+    delay(500);
+  }//while
 }//setup()
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  unsigned long strt = millis();
+
   handleStatusFlash();
+  pollPressureSensor();
 
   if (Serial.available()) {
     byte in = Serial.read();
@@ -144,9 +203,9 @@ void loop() {
     if (lvl == 2) {fireMatch2();}
   }
 
-  Serial.print(getContinuityMatch1());
+  Serial.print(getTemp());
   Serial.print(F(", "));
-  Serial.println(getContinuityMatch2());
+  Serial.println(getPressure());
 
   //Serial.print("VRef=");
   //Serial.print(measureVref(500));
@@ -154,6 +213,8 @@ void loop() {
   //Serial.print(readBatteryVoltage());
   //Serial.println("mV");
   //delay(1000);
+
+  Serial.println(millis()-strt);
 
 }//loop()
 
