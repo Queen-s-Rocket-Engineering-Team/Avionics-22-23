377b529c6f0d1c5c95ff724d143af968363a9c2a
Kenneract <37283040+Kenneract@users.noreply.github.com>
2023-05-23 16:11:10 -0400

    Simple status LED flash system
    Implemented non-blocking LED flashing system. Run setStatusFlash() to make onboard LED flash a given number of times.

--- Altimeter_Firmware/Altimeter_Firmware.ino
+++ Altimeter_Firmware/Altimeter_Firmware.ino
@@ -1,9 +1,58 @@
 #define BATT_SENSE_PIN PA2
+#define STATUS_LED_PIN PA0
 
 // Battery sense voltage divider ratio (default 3.00x)
 const float BATT_SENSE_MULTIPLIER = 3.012; //199.4k & 99.1k
 // Internal Vref voltage (default 1200mV)
 const long INT_REF = 1196;
+
+
+// Variables; flash status code
+const unsigned int STATUS_FLASH_COOLDOWN = 1000;  // Pause duration between status flash sequences
+unsigned int flashTime = 190;                     // Time (ms) to hold flashes
+unsigned long lastFlashChange = 0;                // Last millis() timestamp that a flash change occured
+byte statusFlashRemain = 0;                       // Number of status flashes remaining in current cycle
+byte statusCode = 0;                              // Status LED will flash this many times
+
+
+/*
+ * Sets the status LED to flash the
+ * given number of times.
+ */
+void setStatusFlash(byte code) {
+  statusCode = code;
+  lastFlashChange = millis();
+  digitalWrite(STATUS_LED_PIN, LOW);
+}//setStatusFlash()
+
+/*
+ * Handles the status LED flash sequence.
+ * Non-blocking; should be run in loop().
+ */
+void handleStatusFlash() {
+  if (statusCode != 0) {
+    if (statusFlashRemain == 0) {
+      if (millis() - lastFlashChange > STATUS_FLASH_COOLDOWN) {
+        // Reset flash sequence
+        statusFlashRemain = statusCode;
+      }//if
+    } else {
+      // Play flash sequence
+      if (millis() - lastFlashChange > flashTime) {
+        lastFlashChange = millis();
+        // Change flash state
+        if (!digitalRead(STATUS_LED_PIN)) {
+          digitalWrite(STATUS_LED_PIN, HIGH);
+        } else {
+          digitalWrite(STATUS_LED_PIN, LOW);
+          statusFlashRemain--; // one flash complete
+        }//if (status not on)
+      }//if (flash time)
+    }//if (flashses remaining)
+  }//if (code>0
+}//handleStatusFlash()
+
+
 
 /*
  * NOTES:
@@ -12,12 +61,17 @@ const long INT_REF = 1196;
  * resistor divider limits charge speed).
  * See https://github.com/stm32duino/Arduino_Core_STM32/issues/651
  * for options.
+ * 
+ * This file is going to get messy for a while until I clean
+ * it up some more
  */
+
 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(BATT_SENSE_PIN, INPUT_ANALOG);
+  pinMode(STATUS_LED_PIN, OUTPUT);
   analogReadResolution(12);
 
   Serial.begin(9600); //Baud doesn't matter for STM32 USB
@@ -26,12 +80,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.print("VRef=");
-  Serial.print(measureVref(500));
-  Serial.print("mV  VBatt=");
-  Serial.print(readBatteryVoltage());
-  Serial.println("mV");
-  delay(1000);
+  handleStatusFlash();
+
+  if (Serial.available()) {
+    byte in = Serial.read();
+    while (Serial.available()) {Serial.read();}
+    byte lvl = in - '0';
+    setStatusFlash(lvl);
+    Serial.println(statusCode);
+  }
+
+  //Serial.print("VRef=");
+  //Serial.print(measureVref(500));
+  //Serial.print("mV  VBatt=");
+  //Serial.print(readBatteryVoltage());
+  //Serial.println("mV");
+  //delay(1000);
 
 }//loop()
 
