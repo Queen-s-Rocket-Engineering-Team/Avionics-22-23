3d43e4fb4376b79b93df8217a868d79096b36da8
FWILKY <124524439+FWILKY@users.noreply.github.com>
2023-05-27 20:23:02 -0400

    Update Altimeter_Firmware.ino
    I think it would be better if we did launch detection on an average velocity basis if we are just using altitude readings. This is just a basic format but still susceptible to erroneous readings. in future: take average value for pressure readings instead of just one (what is the best way to implement that?).

--- Altimeter_Firmware/Altimeter_Firmware.ino
+++ Altimeter_Firmware/Altimeter_Firmware.ino
@@ -4,42 +4,19 @@
 uint8_t launchState = 0; //Assume unlaunched
 
 MS5x barometer(&Wire); //I do not understand what &Wire means but I know this has something to do with identifying the I2C pins
-Serial.begin(9600); //Tentative
 
-void launchStatus() { //Uses barometer only, accelerometer definitely better for this application, also bad algorithm in general
-  uint16_t alttemp1 = 1; //Set higher than second measured altitude to ensure that second altitude is at least 1m higher on average
-  uint16_t alttemp2 = 0; 
-  uint8_t countr1 = 0;
-  uint8_t countr2 = 0;
-  for (uint8_t i = 0; i < 50; i++)  { //Tries to take 50 altitude value average
-    barometer.checkUpdates();
-    if (barometer.isReady()) {
-      alttemp1 += barometer.getAltitude();
-      countr1 += 1;
-    }
-  }
-  alttemp1 /= countr1;
-
-  delay(100) //delay to allow significant altitude increase
-
-  for (uint8_t i = 0; i < 50; i++)  { //Tries to take 50 altitude value average
-    barometer.checkUpdates();
-    if (barometer.isReady()) {
-      alttemp2 = barometer.getAltitude();
-      countr2 += 1;
-    }
-  }
-  alttemp2 /= countr2;
-
-  if (alttemp2 > alttemp1) { //Compare altitude values to see if there was a positive change (in flight) or not (on pad)
+void launchStatus() { //Uses barometer only, accelerometer definitely useful for this application
+  uint32_t time1 = millis();
+  barometer.checkUpdates();
+  uint16_t alt1 = barometer.getAltitude();
+  delay(100); //Placeholder, could be made smaller/larger
+  barometer.checkUpdates();
+  uint16_t alt2 = barometer.getAltitude();
+  uint16_t deltat = millis() - time1;
+  avgV = (alt1 - alt2) / deltat
+  if (avgV > 10) { //Placeholder avgV threshold, could be made smaller/larger
     launchState = 1;
-    Serial.println("Rocket in flight")
   } else {
     launchState = 0;
-    Serial.println("Rocket not in flight")
   }
-}
-
-void launchStatusAccelerometer() {
-
 }
