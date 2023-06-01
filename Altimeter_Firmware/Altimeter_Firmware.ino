df4d405e27ec559a2bf4c396f2172671bc7f74a6
FWILKY <19fmw1@queensu.ca>
2023-06-01 17:00:49 -0400

    Added altitude calculation, changed library to MS5xxx
    Created "calcAlt()" and "zeroAlt()" which ignore sea level pressures to give just the altitude difference between the pad and rocket while in flight. Fully moved to the MS5xxx library, but not 100% on the proper integration. I think the logic is actually usable but need to test on actual board. Also need to integrate averaging system on readings to improve resistance to spikes in temp/pressure.

--- Altimeter_Firmware/Altimeter_Firmware.ino
+++ Altimeter_Firmware/Altimeter_Firmware.ino
@@ -1,20 +1,41 @@
 #include <Wire.h>
-#include <MS5x.h> //COMMENT OUT LINE 24 IN HEADER FILE, THIS LIBRARY CAN BE FOUND ON THE LIBRARY MANAGER
+#include <MS5xxx.h>
 
-uint8_t launchState = 0; //Assume unlaunched
+uint1_t launchState = 0;  //0 for unlaunched, 1 for launched
+double pnaught = 0;
 
-MS5x barometer(&Wire); //I do not understand what &Wire means but I know this has something to do with identifying the I2C pins
+MS5xxx barometer(&Wire);  //I do not understand what &Wire means but I know this has something to do with identifying the I2C pins
 
-void launchStatus() { //Uses barometer only, accelerometer definitely useful for this application
+void calcAlt() {
+  if (launchState == 1) {
+    if (barometer.connect() == 0) {
+      barometer.ReadProm();
+      barometer.Readout();
+      double temp = (barometer.GetTemp()) + 273;                           //Get temperature reading, convert from celcius to kelvin
+      double pcurrent = (barometer.GetPres()) * 100;                       //Get pressure, convert to Pa from mbar
+      return ((pow(pnaught / pcurrent, 0.19022256) - 1.) * temp) / 0.0065  //This is called the "Hypsometric Formula" and should give the delta altitude (in m) if pnaught is set properly
+    }
+  }
+}
+
+void zeroAlt() {
+  if (launchState == 0) {
+    if (barometer.connect() == 0) {
+      barometer.ReadProm();
+      barometer.Readout();
+      pnaught = (barometer.GetPres()) * 100;  //Get pressure, convert to Pa from mbar
+    }
+  }
+}
+
+void launchStatus() {  //Uses barometer only, accelerometer definitely useful for this application
   uint32_t time1 = millis();
-  barometer.checkUpdates();
-  uint16_t alt1 = barometer.getAltitude();
-  delay(100); //Placeholder, could be made smaller/larger
-  barometer.checkUpdates();
-  uint16_t alt2 = barometer.getAltitude();
+  float alt1 = calcAlt();
+  delay(100);  //Placeholder, could be made smaller/larger
+  float alt2 = calcAlt();
   uint16_t deltat = millis() - time1;
-  avgV = (alt1 - alt2) / deltat
-  if (avgV > 10) { //Placeholder avgV threshold, could be made smaller/larger
+  avgV = (alt1 - alt2) / (deltat/1000); //Calculates average velocity in m/s
+  if (avgV > 10) {  //Placeholder avgV threshold (m/s), could be made smaller/larger
     launchState = 1;
   } else {
     launchState = 0;
